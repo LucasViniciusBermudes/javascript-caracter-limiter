c721e4d4dc252f3dfe23aec44b30c1eb267a880d
Lucas Vinicius Bermudes <lucasviniciusbermudes@gmail.com>
2025-07-28 17:56:53 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const input = document.querySelector("input");
+const counter = document.querySelector('span');
+
+counter.style.color = 'red';
+
+input.addEventListener('input', () => {
+    if (input.value.length < 20) {
+        counter.innerText = input.value.length;
+    } else {
+        counter.innerText = '20';
+        input.value = input.value.substring(0, 20);
+    }
+})
+
+const str = ("Hello, Word!")
+
+const substrings = str.substring(7, 13);
+console.log(substrings)
